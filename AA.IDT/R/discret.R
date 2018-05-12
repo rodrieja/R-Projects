9fd86edca33b3d52304c344e28914bb216cc5051
Gonzalo Piotti <gonzalopiotti@hotmail.com>
2018-05-12 20:43:24 -0300

    agregdo discret supervisado

--- AA.IDT/R/discret.R
+++ AA.IDT/R/discret.R
@@ -2,7 +2,14 @@ library(arules)
 library(AA.IDT)
 library(dplyr)
 library(ggplot2)
+library(arc)
 
+colnames(TelcoChurn)
+str(TelcoChurn)
+grep("TotalCharges", colnames(TelcoChurn))
+grep("Churn", colnames(TelcoChurn))
+TelcoChurn[,19]
+discrNumeric(TelcoChurn, "Churn")
 
 #columnas = vector de columnas continuas
 #estrategia "igual_ancho" o "igual_cantidad"
@@ -20,7 +27,7 @@ discret <- function (dataset, columnas, estrategia, bins){
   return(dataset)
 }
 
-
+?discretize
 # ----------------------------------------------------
 #cargo el dataset completo, porque hay que discretizar antes de dividir en test-validacion
 TelcoChurn <- read.csv('Telcochurn.csv')
@@ -52,8 +59,28 @@ for (method in c("igual_ancho", "igual_freq")) {
   }
 }
 
+#corrida supervisada
+data <- discrNumeric(TelcoChurn, "Churn")
+#separo train de validacion
+set.seed(2018)
+train <- sample_frac(data$Disc.data, trn_size)
+sid <- as.numeric(rownames(train)) # because rownames() returns character
+validation <- data$Disc.data[-sid,]
+
+# ejecuto las corridas en base al CF
+results.cf = execute.J48(ConfidenceFactor.Vector,
+                         0,
+                         train,
+                         validation)
+
+results = rbind(results,
+                cbind(results.cf, Bins = 0, Method = "supervisado"))
+}
+levels(data$Disc.data$MonthlyCharges)
+
 data.igual.ancho <- results[results$Method == "igual_ancho",]
 data.igual.freq <- results[results$Method == "igual_freq",]
+data.supervisado <- results[results$Method == "supervisado",]
 # ------------------------------------------------- TAMAÑO HOJAS -------------------------------------------------
 
 
@@ -135,3 +162,31 @@ ggplot(data = data.igual.freq) + geom_point(size = 2, alpha=0.7, aes(x = Bins, y
 ggsave("accuracy_train_vs_validacion_igual_freq.png", dpi=400, height=6, width=12)
 
 
+# ------------------------------------------------- ACCURACY supervisado VS no supervisado  -------------------------------------------------
+ggplot(data = data.igual.ancho, aes(x = Bins, y = ValidationAccuracy, colour = factor(ConfidenceFactor))) +
+  geom_line(size = 1) + geom_point(shape = 16, size = 1.5) +
+  scale_x_continuous(breaks = data.igual.ancho$Bins) +
+  ggtitle("Acc Validation | Bins = Igual Ancho") +
+  theme(plot.title = element_text(hjust = 0.5))
+ggsave("accuracy_validacion_igual_ancho.png", dpi=400, height=6, width=12)
+
+#busco los mejores Bins de cada tipo (supervisado es el unico que hay)
+best.bins.igual.ancho <- data.igual.ancho[data.igual.freq$ValidationAccuracy == max(data.igual.ancho$ValidationAccuracy),"Bins"]        
+best.bins.igual.freq <- data.igual.freq[data.igual.freq$ValidationAccuracy == max(data.igual.freq$ValidationAccuracy),"Bins"]        
+best.supervisado <- data.supervisado[data.supervisado$ValidationAccuracy == max(data.supervisado$ValidationAccuracy),]        
+
+comp.igual.ancho <- data.igual.ancho[data.igual.ancho$Bins == best.bins.igual.ancho,]
+comp.igual.freq <- data.igual.freq[data.igual.freq$Bins == best.bins.igual.freq,]
+comp.supervisado <- data.supervisado
+
+
+data.comparacion <- rbind(comp.igual.ancho, comp.igual.freq, comp.supervisado)
+data.comparacion
+ggplot(data = data.comparacion, aes(x = ConfidenceFactor, y = ValidationAccuracy, colour = factor(Method) )) +
+geom_line(size = 1) + geom_point(shape = 16, size = 1.5) +
+  scale_x_continuous(breaks = data.comparacion$ConfidenceFactor) +
+  ggtitle("Acc Validation | Comparacion Métodos (mejores corridas)") +
+  theme(plot.title = element_text(hjust = 0.5)) +
+  labs(color="Método") 
+  
+ggsave("accuracy_validacion_comparacion_metodos.png", dpi=400, height=6, width=12)
